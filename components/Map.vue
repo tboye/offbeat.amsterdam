e75729e32eaa2435b153efd80a19e2777e2c6d6b
sedum <sedum@oziosi.org>
2022-07-29 19:19:50 +0200

    fix markers not displayed in prod

--- components/Map.vue
+++ components/Map.vue
@@ -24,6 +24,14 @@ import "leaflet/dist/leaflet.css";
 import { LMap, LTileLayer, LMarker } from 'vue2-leaflet';
 import dayjs from 'dayjs';
 import { mapActions, mapState } from 'vuex'
+import { Icon } from 'leaflet';
+
+delete Icon.Default.prototype._getIconUrl;
+Icon.Default.mergeOptions({
+  iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
+  iconUrl: require('leaflet/dist/images/marker-icon.png'),
+  shadowUrl: require('leaflet/dist/images/marker-shadow.png'),
+});
 
 export default {
    components: {
