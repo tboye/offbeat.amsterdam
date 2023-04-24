1b8f32c335b599f75802cd6a928ef9830ab0a985
lesion <lesion@autistici.org>
2023-04-24 22:54:49 +0200

    restore admin tab bar horizontally

--- pages/Admin.vue
+++ pages/Admin.vue
@@ -4,7 +4,7 @@ v-container.container.pa-0.pa-md-3
     v-alert(v-if='url!==settings.baseurl' outlined type='warning' show-icon :icon='mdiAlert')
       span(v-html="$t('admin.wrong_domain_warning', { url, baseurl: settings.baseurl })")
 
-    v-tabs(v-model='selectedTab' show-arrows :next-icon='mdiChevronRight' :prev-icon='mdiChevronLeft' vertical)
+    v-tabs(v-model='selectedTab' show-arrows :next-icon='mdiChevronRight' :prev-icon='mdiChevronLeft')
 
       //- SETTINGS
       v-tab(href='#settings') {{$t('common.settings')}}
