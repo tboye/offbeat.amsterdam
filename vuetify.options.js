079500dd8c26fedfc0eb1e89f7e55cd228e65a94
lesion <lesion@autistici.org>
2024-09-25 23:24:10 +0200

    minor

--- vuetify.options.js
+++ vuetify.options.js
@@ -1,15 +1,16 @@
 // const minifyTheme = require('minify-css-string').default
+// import { ca, cs, de, en, es, fr, it, nl, pl, pt, tr, sk, sv, ro, ru, zhHans  } from 'vuetify/lib/locale/index.mjs'
 import { ca, cs, de, en, es, eu, fr, gl, it, nb, nl, pl, pt, tr, sk, sv, ro, ru, zhHans  } from 'vuetify/lib/locale'
 
 import { aliases, mdi } from 'vuetify/iconsets/mdi'
 export default (ctx) => {
-    console.error(ctx)
 
 //   const settings = process.server ? (res.locals.settings || {}) : nuxtState.state.settings || {}
 //   const is_dark = nuxtState?.state?.localSettings['theme.is_dark'] ?? settings['theme.is_dark']
 
   return {
     lang: { locales: { ca, cs, de, en, es, eu, fr, gl, it, nb, nl, pl, pt, sk, sv, tr, ro, ru, zhHans } },
+    // lang: { locales: { ca, cs, de, en, es, fr, it, nl, pl, pt, sk, sv, tr, ro, ru, zhHans } },
     theme: {
       options: {
         customProperties: false,
