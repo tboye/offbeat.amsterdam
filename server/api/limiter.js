dbbe904785068b2fcaeb1730062a185c5a43ea19
tboye <22368316+tboye@users.noreply.github.com>
2025-01-31 14:19:33 +0100

    Disable API rate limits for admins (#5)
    OFF-73

--- server/api/limiter.js
+++ server/api/limiter.js
@@ -1,35 +1,37 @@
-const rateLimit = require('express-rate-limit')
-const log = require('../log')
+const rateLimit = require('express-rate-limit');
+const log = require('../log');
 
-const next = (req, res, next) => next()
+const createRateLimiter = (options) => (req, res, next) => {
+  if (process.env.NODE_ENV !== 'production' || (req.user && req.user.is_admin && req.user.is_active)) {
+    return next(); // Disable rate limit for admins or in non-production mode
+  }
+  return rateLimit(options)(req, res, next);
+};
 
 const instanceApiRateLimiter = {
-
-  DDOSProtectionApiRateLimiter: (process.env.NODE_ENV !== 'production' ? next : rateLimit({
-    windowMs: 60 * 1000, // 1 minutes
-    max: 250, // Limit each IP to 150 requests per `window`
+  DDOSProtectionApiRateLimiter: createRateLimiter({
+    windowMs: 60 * 1000, // 1 minute
+    max: 250, // Limit each IP to 250 requests per `window`
     standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
     legacyHeaders: false, // Disable the `X-RateLimit-*` headers
     handler: (request, response, next, options) => {
-      log.warn(`DDOS protection api rate limiter: > 250req/minute/ip ${request.ip}`)
-      return response.status(options.statusCode).send(options.message)    
-    }
-  })),
-
+      log.warn(`DDOS protection API rate limiter: > 250req/minute/ip ${request.ip}`);
+      return response.status(options.statusCode).send(options.message);
+    },
+  }),
 
   /** This is a limiter used to avoid spam
    * (used during the registration, pass recovery, posting events) */
-  SPAMProtectionApiRateLimiter: (process.env.NODE_ENV !== 'production' ? next : rateLimit({
+  SPAMProtectionApiRateLimiter: createRateLimiter({
     windowMs: 5 * 60 * 1000, // 5 minutes
-    max: 6, // Limit each IP to 3 requests per `window` (here, per 5 minutes)
+    max: 6, // Limit each IP to 6 requests per 5 minutes
     standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
     legacyHeaders: false, // Disable the `X-RateLimit-*` headers
     handler: (request, response, next, options) => {
-      log.warn(`SPAM protection api rate limiter: 6req/5min/ip ${request.ip}`)
-      return response.status(options.statusCode).send(options.message)    
-    }
-  }))
+      log.warn(`SPAM protection API rate limiter: 6req/5min/ip ${request.ip}`);
+      return response.status(options.statusCode).send(options.message);
+    },
+  }),
+};
 
-}
-
-module.exports = instanceApiRateLimiter
+module.exports = instanceApiRateLimiter;
