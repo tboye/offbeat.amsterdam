a5e8f3ea92f5439f3c1a148853df06e887f46421
Tatiana Boyé <tatiana.boye@me.com>
2025-02-08 14:41:06 +0100

    Adjust source link
    OFF-26

--- components/Footer.vue
+++ components/Footer.vue
@@ -26,7 +26,7 @@ v-footer(aria-label='Footer')
 
   v-btn.ml-1(v-if='settings.enable_federation' color='primary' text rel='me' @click.prevent='showFollowMe=true') {{$t('event.interact_with_me')}}
   v-spacer
-  v-btn(color='primary' text href='https://gancio.org' target='_blank' rel="noopener") Gancio <small>{{settings.version}}</small>
+  v-btn(color='primary' text href='https://github.com/tboye/offbeat.amsterdam' target='_blank' rel="noopener") offbeat.amsterdam {{settings.version}}
 
 </template>
 <script>
