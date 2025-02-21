7676d90fb5abd5d1fea63a4854c426c31cd94c2e
sedum <sedum@oziosi.org>
2025-02-21 02:13:52 +0100

    fix: minor refactor add SearchAddress component

--- server/helpers/geolocation/index.js
+++ server/helpers/geolocation/index.js
@@ -4,15 +4,7 @@ const photon = require('../../../server/services/geocoding/photon')
 const geocodingProviders = [ nominatim, photon ]
 
 const geolocation = {
-  getGeocodingProvider(providerName) {
-    let geocodingProvider
-    geocodingProviders.forEach((item) => {
-      if (item.commonName === providerName) {
-        geocodingProvider = item
-      }
-    })
-    return geocodingProvider
-  }
+  getGeocodingProvider(providerName) { return geocodingProviders.find(i => i.commonName === providerName) }
 }
 
 module.exports = geolocation
