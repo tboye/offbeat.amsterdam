8540f4a525554483cf21f0b8373bd40912092e17
lesion <lesion@autistici.org>
2025-03-06 18:26:41 +0100

    fix(style): increase min Editor heigth (page, announcement, about, event)

--- pages/about.vue
+++ pages/about.vue
@@ -1,7 +1,7 @@
 <template lang="pug">
 v-container
   v-card-text(v-if='$auth.user && $auth.user.is_admin')
-    Editor.px-3.ma-0(v-model='about' :label="$t('common.about')")
+    Editor.px-3.ma-0(v-model='about' :label="$t('common.about')" height='500px')
   v-card-text(v-else v-html='about')
   v-card-actions(v-if='$auth.user && $auth.user.is_admin')
     v-spacer
