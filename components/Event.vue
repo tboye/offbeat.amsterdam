3456a221c67e022fcbc82cc02c067d2c9b74b09f
Tatiana Boyé <tatiana.boye@me.com>
2025-07-14 20:22:01 +0200

    Remove itemprops
    OFF-273

--- components/Event.vue
+++ components/Event.vue
@@ -1,11 +1,11 @@
 <template>
-<article class='h-event' :class="{ 'is-past': isPast }" itemscope itemtype="https://schema.org/Event">
+<article class='h-event' :class="{ 'is-past': isPast }">
 
-  <nuxt-link :to='`/event/${event.slug || event.id}`' itemprop="url">
+  <nuxt-link :to='`/event/${event.slug || event.id}`'>
     <MyPicture v-if='!hide_thumbs' :event='event' thumb :lazy='lazy' />
     <v-icon class='float-right mt-1 mr-1' v-if='event.parentId' color='success' v-text='mdiRepeat' />
     <v-icon class='float-right mt-1 mr-1' v-if='isPast' color='warning' v-text='mdiTimerSandComplete'/>
-    <h2 class='title p-name' itemprop="name">{{ event.title }}</h2>
+    <h2 class='title p-name'>{{ event.title }}</h2>
   </nuxt-link>
 
   <v-img contain v-if='event?.ap_user?.image' :src='event?.ap_user?.image' max-height=30  max-width=30 style="position: absolute; top: 5px; right: 5px;" />
@@ -13,18 +13,18 @@
   <v-card-text class='body pt-0 pb-0'>
 
     <time class='dt-start subtitle-1' :datetime='$time.unixFormat(event.start_datetime, "yyyy-MM-dd HH:mm")'
-      itemprop="startDate" :content="$time.unixFormat(event.start_datetime, 'yyyy-MM-dd\'T\'HH:mm')"> <v-icon v-text='mdiCalendar' /> {{ $time.when(event) }}
+      :content="$time.unixFormat(event.start_datetime, 'yyyy-MM-dd\'T\'HH:mm')"> <v-icon v-text='mdiCalendar' /> {{ $time.when(event) }}
     </time>
-    <time class='d-none dt-end' v-if='event.end_datetime' itemprop="endDate"
+    <time class='d-none dt-end' v-if='event.end_datetime'
       :content="$time.unixFormat(event.end_datetime,'yyyy-MM-dd\'T\'HH:mm')"> {{ $time.unixFormat(event.end_datetime)}}</time>
 
-    <div class='p-location' itemprop="location" itemscope itemtype="https://schema.org/Place">
+    <div class='p-location'>
       <nuxt-link class='place d-block pl-0' text
         :to='`/place/${event.place.id}/${encodeURIComponent(event.place.name)}`'>
         <v-icon v-text='mdiMapMarker'></v-icon>
-        <span itemprop='name'>{{ event.place.name }}</span>
+        <span>{{ event.place.name }}</span>
       </nuxt-link>
-      <div class='d-none p-street-address' itemprop='address'>{{ event.place.address }}</div>
+      <div class='d-none p-street-address'>{{ event.place.address }}</div>
     </div>
   </v-card-text>
 
